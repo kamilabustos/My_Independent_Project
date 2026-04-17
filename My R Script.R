ecd91865775252d815f2fd082be14c034b0f07c5
kamilabustos <kamiladbustos@gmail.com>
2026-04-17 13:35:01 -0700

    Add files via upload

---
+++ My R Script.R
@@ -0,0 +1,12 @@
+my_object<-2/4*3
+MyVector<-c(1,2,3)
+my_string_vector<-c("this","is","a","vector")
+mean
+mean(MyVector)
+mean(my_string_vector)
+#this is a comment
+MyVector[3]
+#to look at one variable in a data set, you do name of data frame then $ than the variable
+#lets say a bunch of things pop up once you do that, then you do [] and the number
+#ie. covid_tweets$tweet_text[3]
+#that will get you the third variable
